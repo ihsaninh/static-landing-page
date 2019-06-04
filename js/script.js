e66d459c78edefd32d9e3a5460b2d60bcdbe9aa4
Ihsan Nurul Habib <ihsan.inh@gmail.com>
2019-06-04 22:30:06 +0700

    add responsive

--- js/script.js
+++ js/script.js
@@ -1,22 +1,11 @@
 $(document).ready(function() {
 
 	$(window).scroll(function() {
-		const scroll = $(this).scrollTop();
-
-		// image show 
-		if(scroll > $('.gallery').offset().top - 400) {
-			$('.gallery .thumb').each(function(i) {
-				setTimeout(function() {
-					$('.gallery .thumb').eq(i).addClass('show');
-				}, 100 * (i + 1));
-			});
-		}
-
+		let scroll = $(this).scrollTop();
 		// discover show
 		if(scroll > 200) {
 			$('.discover-img, .discover-desc').addClass('show');
 		}
-
 		// discover show
 		if(scroll > 100) {
 			$('.navbar').addClass('fixed-top');
@@ -25,29 +14,38 @@ $(document).ready(function() {
 			$('.navbar-nav, .navbar-brand').css('margin-top', '0');
 		} else {
 			$('.navbar').removeClass('fixed-top');
-			$('.nav-link, .navbar-brand').attr('style', 'color: white !important');
-			$('.navbar-nav, .navbar-brand').css('margin-top', '10px');
+			$('.nav-link, .navbar-brand').removeAttr("style");
 		}
-
 		// event show
 		if (scroll > $('.event').offset().top - 500) {
 			$('.event-img, .event-desc').addClass('show');
 		}
-
 		// subscribe show
 		if (scroll > $('.subscribe').offset().top - 500) {
 			$('.subscribe').addClass('show');
 		}
-
 		// location show
 		if (scroll > $('.location').offset().top - 700) {
 			$('.location').addClass('show');
 		}
-
 		// desc gallery
 		if (scroll > $('.desc-gallery').offset().top - 700) {
 			$('.desc-gallery').addClass('show');
 		}
+		// image show 
+		if(scroll > $('.gallery').offset().top - 400) {
+			$('.gallery .thumb').each(function(i) {
+				setTimeout(function() {
+					$('.gallery .thumb').eq(i).addClass('show');
+				}, 100 * (i + 1));
+			});
+		}
+		// progress-bar
+		let windowHeight     = $(window).height();
+		let documentHeight   = $(document).height();
+		let percentagescroll = scroll / (documentHeight - windowHeight);
+		let percentageround  = Math.round(percentagescroll * 100);
+		$('.progress-bar').css({'width' : percentageround + '%'});
 
 	});
 
